de85ac318c6a9e614b401f1bc8695a49d58f4a53
ponsuyambu <suyambu.vps@gmail.com>
2017-05-07 00:51:03 +0200

    One way data binding

--- app/src/main/java/in/ponshere/androiddatabinding/MainActivity.java
+++ app/src/main/java/in/ponshere/androiddatabinding/MainActivity.java
@@ -1,13 +1,18 @@
 package in.ponshere.androiddatabinding;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import in.ponshere.androiddatabinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        User user = new User("Test", "User");
+        binding.setUser(user);
     }
 }
---
+++ app/src/main/java/in/ponshere/androiddatabinding/User.java
@@ -0,0 +1,15 @@
+package in.ponshere.androiddatabinding;
+
+/**
+ * @author Ponsuyambu
+ * @since 7/5/17.
+ */
+
+public class User {
+    public final String firstName;
+    public final String lastName;
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
